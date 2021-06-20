e9501a7d45c5afca2a543dd0006bd58535f87bd9
Sharni Zaugg <sharni.greener@gmail.com>
2021-06-19 20:37:01 -0600

    add dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Post, Comment} = require('../models');
 
+
+//homepage, display all posts
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
@@ -39,11 +41,53 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
-    //query database to get all posts before res.render
-})
 
+//dashboard, create post if logged in
+router.get('/dashboard', (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+        'id',
+        'title',
+        'contents', 
+        'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User, 
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true}));
+            res.render('dashboard', {
+                posts, 
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    //query database to get all posts before res.render
+});
+
+
+//place code here to edit posts
+
+//login 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
         res.redirect('/');
@@ -53,9 +97,54 @@ router.get('/login', (req, res) => {
     res.render('login')
 })
 
+//single post page
 router.get('/post/:id', (req, res) => {
-    res.render('single-post')
-})
+    Post.findOne({
+        where: {
+            id : req.params.id
+        },
+        attributes: [
+            'id', 
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+     .then(dbPostData => {
+         if (!dbPostData) {
+             res.status(400).json({ message: 'No post found with this id'});
+             return;
+         }
+ 
+         //serialize the data
+         const post = dbPostData.get({ plain: true });
+ 
+         //pass data to template
+         res.render('single-post', { 
+           post,
+           loggedIn: req.session.loggedIn
+         });
+     })
+     .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+     });
+ });
 
 
 module.exports = router;
---
+++ public/javascript/add-post.js
@@ -0,0 +1,24 @@
+async function addPostHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const contents = document.querySelector('textarea[name="post-content"]').value;
+
+    const response = await fetch('/api/posts', {
+        method: 'post',
+        body: JSON.stringify({
+            title, 
+            contents
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector(".new-post").addEventListener("submit", addPostHandler);
